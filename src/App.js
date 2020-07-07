c6b1d189b35904b4b09e762575fe14601341ff97
Leah Heming <heming.alexander@gmail.comgit>
2020-07-07 16:13:35 +0200

    update chatt-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './components/Contact.css';
+import Contact from './components/Contact.js';
+
+function App() {
+  return (
+    <div>
+      <Contact />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="Contact">
+            <div className="avatar"></div>
+            <div className="contact-info">
+                <div className="name">Kim Simpson</div>
+                <div className="status">
+                    <div className="status-online"></div>
+                    <div className="status-text"></div>            
+                </div>
+            </div>    
+        </div>
+    )
+}
+
+
+export default Contact;
